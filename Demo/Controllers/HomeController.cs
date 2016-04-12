366cbb8a52f8459d5526c2cb6e42b7b6787ef1ce
HarperJeremy <harper@stimulantgroup.com>
2016-04-12 12:29:42 -0500

    Upload New

---
+++ Demo/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcInfiniteScrollGridDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public HomeController()
+        {
+            
+        }
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("GetNext");
+        }
+
+        public ActionResult GetNext(int? pageNum)
+        {
+            pageNum = pageNum ?? 0;
+            ViewBag.IsEndOfRecords = false;
+            if (Request.IsAjaxRequest())
+            {
+                switch (pageNum.Value)
+                    {
+                    case 1:
+                            return PartialView("_Bio");
+                            break;
+                    case 2:
+                            return PartialView("_Page2");
+                            break;
+                    case 3:
+                            return PartialView("_Page3");
+                            break;
+                    case 4:
+                            return PartialView("_Page4");
+                            break;
+                    case 5:
+                            return PartialView("_Page5");
+                            break;
+                    default:
+                            return PartialView("_Bio");
+                            break;
+                    }
+                
+            }
+            else
+            {
+
+                return View("Index");
+            }
+        }
+
+
+    }
+}
